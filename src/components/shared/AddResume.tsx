d9323dc2e85498d8313fdb274bba4f09d1b7d27c
Elena Skorobogatova <skorobogatovamail@gmail.com>
2025-07-24 19:55:16 +0300

    add dialog

--- src/components/shared/AddResume.tsx
+++ src/components/shared/AddResume.tsx
@@ -1,18 +1,47 @@
 import { PlusSquare } from 'lucide-react';
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { Button } from '../ui/button';
 
 type AddResumeProps = {};
 
 const AddResume: React.FC<AddResumeProps> = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div>
       <div
-        className="p-14 py-24 border items-center flex justify-center 
+        className="p-14 py-24 border-dashed items-center flex justify-center 
       bg-secondary rounded-lg h-[280px] hover:scale-105 transition-all 
       hover:shadow-md cursor-pointer"
+        onClick={() => setIsOpen(!isOpen)}
       >
         <PlusSquare />
       </div>
+
+      <Dialog open={isOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your account and remove
+              your data from our servers.
+            </DialogDescription>
+            <div className="flex justify-end gap-2">
+              <Button variant="secondary" onClick={() => setIsOpen(false)}>
+                Cancel
+              </Button>
+              <Button>Create</Button>
+            </div>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 };
