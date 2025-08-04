b51e1f2aee8bf1e4facbfde70c4e8133c76d5c62
Elena Skorobogatova <skorobogatovamail@gmail.com>
2025-08-04 23:48:56 +0300

    add personalDetailForm

--- src/components/pages/EditResume/ResumeForm/components/PersonalDetailForm.tsx
+++ src/components/pages/EditResume/ResumeForm/components/PersonalDetailForm.tsx
@@ -1,12 +1,65 @@
-import React from 'react';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { ResumeInfoContext } from '@/context/ResumeInfoContext';
+import React, { useContext } from 'react';
 
-type PersonalDetailFormProps = {};
+const PersonalDetailForm: React.FC = () => {
+  const context = useContext(ResumeInfoContext);
+  if (!context) return null;
+  const { resumeInfo, setResumeInfo } = context;
 
-const PersonalDetailForm: React.FC<PersonalDetailFormProps> = () => {
+  const handleInputChange = (e: HTMLInputElement) => {
+    console.log(resumeInfo);
+    if (resumeInfo && setResumeInfo) {
+      setResumeInfo({ ...resumeInfo, [e.name]: e.value });
+    }
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(resumeInfo);
+  };
+
   return (
     <div className="rounded-lg border-t-4 border-primary p-4 shadow-md">
       <h2 className="text-lg font-bold">Personal Detail</h2>
       <p>Get started with the basic information</p>
+
+      <form className="grid grid-cols-2 gap-4" onSubmit={handleSubmit}>
+        <div>
+          <label>First Name</label>
+          <Input
+            name="firstName"
+            required
+            placeholder="John"
+            onChange={(e) => handleInputChange(e.target)}
+          ></Input>
+        </div>
+        <div>
+          <label>Last Name</label>
+          <Input name="lasttName" required placeholder="Doe"></Input>
+        </div>
+        <div>
+          <label>Job Title</label>
+          <Input name="jobTitle" required placeholder="Sowtware Developer"></Input>
+        </div>
+        <div>
+          <label>Address</label>
+          <Input name="address" required placeholder="Los Angeles"></Input>
+        </div>
+        <div>
+          <label>Email</label>
+          <Input name="email" type="email" required placeholder="email@example.com"></Input>
+        </div>
+        <div>
+          <label>Phone</label>
+          <Input name="phone" type="phone" required placeholder="+1 234 567 "></Input>
+        </div>
+
+        <div className="mt-3 flex justify-end">
+          <Button type="submit">Save</Button>
+        </div>
+      </form>
     </div>
   );
 };
--- src/components/pages/EditResume/ResumePreview/components/SkillsPreview.tsx
+++ src/components/pages/EditResume/ResumePreview/components/SkillsPreview.tsx
@@ -12,7 +12,7 @@ const SkillsPreview: React.FC<SkillsPreviewProps> = ({ resumeInfo }) => {
           color: resumeInfo?.themeColor,
         }}
       >
-        Education
+        Skills
       </h2>
       <hr
         style={{
@@ -22,14 +22,14 @@ const SkillsPreview: React.FC<SkillsPreviewProps> = ({ resumeInfo }) => {
 
       <div className="grid grid-cols-2 gap-3 my-4">
         {resumeInfo?.skills?.map((skill, index) => (
-          <div key={index} className="flex items-center justify-between">
+          <div key={index} className="flex items-center justify-between gap-2">
             <h2 className="text-xs">{skill.name}</h2>
             <div className="h-2 bg-gray-200 w-[120px]">
               <div
                 className="h-2"
                 style={{
                   backgroundColor: resumeInfo?.themeColor,
-                  width: skill?.rating * 20 + '%',
+                  width: skill?.rating + '%',
                 }}
               ></div>
             </div>
